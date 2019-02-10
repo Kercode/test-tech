fee112fd247b0d5477141bee1c76c571adf88b28
Nicolas Le Chenic <nicolaslechenic@gmail.com>
2019-02-10 09:55:36 +0100

    add $score->points() tests

--- tests/ScoreTest.php
+++ tests/ScoreTest.php
@@ -5,6 +5,7 @@ require('./src/Score.php');
 use PHPUnit\Framework\TestCase;
 
 class ScoreTest extends TestCase {
+
   public function testWithValidScore() {
     $score = new Score('1:2');
 
@@ -17,5 +18,23 @@ class ScoreTest extends TestCase {
 
     $this->assertEquals(false, $tooHighScore->isValid());
     $this->assertEquals(false, $negativeScore->isValid());
+  }
+
+  public function testHeroWinPoints() {
+    $score = new Score('4:2');
+
+    $this->assertEquals(3, $score->points());
+  }
+
+  public function testDrawPoints() {
+    $score = new Score('1:1');
+
+    $this->assertEquals(1, $score->points());
+  }
+
+  public function testHeroLoosePoints() {
+    $score = new Score('0:1');
+
+    $this->assertEquals(0, $score->points());
   }
 }
