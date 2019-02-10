3cec6ba41aeaee57d83054ac92ede41127c6e6ae
Nicolas Le Chenic <nicolaslechenic@gmail.com>
2019-02-10 10:37:03 +0100

    implement $season->total()

--- src/Season.php
+++ src/Season.php
@@ -5,6 +5,18 @@ class Season {
     $this->arrayStringScore = $arrayStringScore;
   }
 
+  public function total() {
+    $total = 0;
+
+    foreach($this->arrayStringScore as $stringScore) {
+      $score = new Score($stringScore);
+
+      $total += $score->points();
+    }
+
+    return $total;
+  }
+
   public function hasValidNumberOfMatches() {
     if (count($this->arrayStringScore) === 10) {
       return true;
