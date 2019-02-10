d18d03a4939d3750db491e365e35d92774738264
Nicolas Le Chenic <nicolaslechenic@gmail.com>
2019-02-10 10:21:37 +0100

    test $season->total() and clean code

--- tests/SeasonTest.php
+++ tests/SeasonTest.php
@@ -6,10 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 class SeasonTest extends TestCase {
   public function testWithValidNumberOfMatches() {
-    $matches = [
-      '0:0', '0:0', '0:0', '0:0', '0:0',
-      '0:0', '0:0', '0:0', '0:0', '0:0'
-    ];
+    $matches = array_fill(0, 10, '0:0');
     $season = new Season($matches);
 
     $this->assertEquals(true, $season->hasValidNumberOfMatches($matches));
@@ -20,5 +17,26 @@ class SeasonTest extends TestCase {
     $season = new Season($matches);
 
     $this->assertEquals(false, $season->hasValidNumberOfMatches($matches));
+  }
+
+  public function testWinAllTotal() {
+    $matches = array_fill(0, 10, '1:0');
+    $season = new Season($matches);
+
+    $this->assertEquals(30, $season->total($matches));
+  }
+
+  public function testDrawAllTotal() {
+    $matches = array_fill(0, 10, '1:1');
+    $season = new Season($matches);
+
+    $this->assertEquals(10, $season->total($matches));
+  }
+
+  public function testLooseAllTotal() {
+    $matches = array_fill(0, 10, '0:1');
+    $season = new Season($matches);
+
+    $this->assertEquals(0, $season->total($matches));
   }
 }
